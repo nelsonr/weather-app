5bfbd3f1a71c58597d313eaccb8bf9570438e5ea
Nelson Rodrigues <nelsonrodrigues@hi-interactive.com>
2023-12-04 21:24:18 +0000

    Add current temperature

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 import { useEffect, useState } from 'react';
-import { getUserGeolocation } from './utils';
+import { getLocalTemperature, getUserGeolocation } from './utils';
 import './App.css'
 
 function App () {
     const [userLocation, setUserLocation] = useState<GeolocationPosition | GeolocationPositionError>()
+    const [currentTemperature, setCurrentTemperature] = useState<number>();
 
     useEffect(() => {
         const tryToGetUserLocation = async () => {
             try {
-                setUserLocation(await getUserGeolocation());
+                const location = await getUserGeolocation();
+                setUserLocation(location);
+
+                return Promise.resolve(location as GeolocationPosition);
             } catch (error) {
                 setUserLocation(error as GeolocationPositionError);
+
+                return Promise.reject(error);
+            }
+        }
+
+        const tryToGetLocalTemperature = async (location: GeolocationPosition) => {
+            try {
+                const temperature = await getLocalTemperature(location);
+
+                setCurrentTemperature(Math.round(temperature));
+            } catch (error) {
+                console.error(error);
             }
         }
 
         if (!userLocation) {
-            tryToGetUserLocation();
+            tryToGetUserLocation()
+                .then((location: GeolocationPosition) => tryToGetLocalTemperature(location))
+                .catch((reason) => console.error(reason));
         }
     }, [userLocation, setUserLocation]);
 
@@ -24,6 +42,7 @@ function App () {
             <div>
                 <strong>Latitude:</strong> {userLocation.coords.latitude}&nbsp;
                 <strong>Longitude:</strong> {userLocation.coords.longitude}
+                <p>Current temperature ouside is {currentTemperature}ºC</p>
             </div>
         );
     } else if (userLocation instanceof GeolocationPositionError) {
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { fetchWeatherApi } from "openmeteo";
+
 export function getUserGeolocation (): Promise<GeolocationPosition | GeolocationPositionError> {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(
@@ -5,4 +7,30 @@ export function getUserGeolocation (): Promise<GeolocationPosition | Geolocation
             (positionError: GeolocationPositionError) => reject(positionError)
         );
     });
+}
+
+export async function getLocalTemperature (location: GeolocationPosition) {
+    const url = "https://api.open-meteo.com/v1/forecast";
+    const params = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        current: "temperature_2m"
+    };
+
+    const responses = await fetchWeatherApi(url, params);
+    const response = responses[0];
+
+    // Attributes for timezone and location
+    const utcOffsetSeconds = response.utcOffsetSeconds();
+    const current = response.current()!;
+
+    // Note: The order of weather variables in the URL query and the indices below need to match!
+    const weatherData = {
+        current: {
+            time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+            temperature: current.variables(0)!.value(),
+        },
+
+    };
+    return weatherData.current.temperature;
 }
